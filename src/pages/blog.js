3ef6b7344cc38aba6c7e53f565c15af2f8875b0d
Saikat Das <saikatdas0790@gmail.com>
2019-05-21 13:33:06 +0530

    Working with Markdown posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,39 @@
 import React from "react";
 import Layout from "../components/layout";
+import { useStaticQuery, graphql } from "gatsby";
 
-const BlogPage = () => (
-  <Layout>
-    <h1>Blog</h1>
-    <p>Posts will show up here later on</p>
-  </Layout>
-);
+const BlogPage = () => {
+  const {
+    allMarkdownRemark: { edges }
+  } = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+            id
+          }
+        }
+      }
+    }
+  `);
+
+  return (
+    <Layout>
+      <h1>Blog</h1>
+      <ol>
+        {edges.map(({ node: { frontmatter: { title, date }, id } }) => (
+          <li key={id}>
+            <h2>{title}</h2>
+            <p>{date}</p>
+          </li>
+        ))}
+      </ol>
+    </Layout>
+  );
+};
 
 export default BlogPage;
